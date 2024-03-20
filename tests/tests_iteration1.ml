8635cda3840d7fda41c763b8a9e2423907b0ed26
Max Charrier <m@lilmaax.fr>
2024-03-20 15:08:35 +0100

    Add some test
    Signed-off-by: Max Charrier <m@lilmaax.fr>

---
+++ tests/tests_iteration1.ml
@@ -0,0 +1,9 @@
+#load "../cambrick/camlbrick.cmo"
+#mod_use "CPtest.ml"
+open CPtest;;
+
+let test_fonc_brick_color_empty () : unit =
+  let res : t_camlbrick_color t_test_result = test_exec (brick_color, "brick_color_empty", ([| [| BK_empty |] |], 0, 0) in
+
+  assert_equals ()
+;;
