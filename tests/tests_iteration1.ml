5dfb5f085290d6c4660d30c70f387f5a73732519
Max Charrier <m@lilmaax.fr>
2024-03-20 15:28:50 +0100

    Test fonction brick_color avec une brique vide
    Signed-off-by: Max Charrier <m@lilmaax.fr>

--- tests/tests_iteration1.ml
+++ tests/tests_iteration1.ml
@@ -5,5 +5,5 @@ open CPtest;;
 let test_fonc_brick_color_empty () : unit =
   let res : t_camlbrick_color t_test_result = test_exec (brick_color, "brick_color_empty", ([| [| BK_empty |] |], 0, 0) in
 
-  assert_equals ()
+    assert_equals (BLACK, test_get res)
 ;;
