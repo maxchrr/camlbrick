322953148d21bb270f1e703d6daf481153a6513c
Max Charrier <m@lilmaax.fr>
2024-03-21 09:21:36 +0100

    Add test report
    Signed-off-by: Max Charrier <m@lilmaax.fr>

--- tests/tests_iteration1.ml
+++ tests/tests_iteration1.ml
@@ -1,9 +1,8 @@
-#load "../cambrick/camlbrick.cmo"
-#mod_use "CPtest.ml"
 open CPtest;;
+open Camlbrick;;
 
 let test_fonc_brick_color_empty () : unit =
-  let res : t_camlbrick_color t_test_result = test_exec (brick_color, "brick_color_empty", ([| [| BK_empty |] |], 0, 0) in
+  let res : t_camlbrick_color t_test_result = test_exec (brick_color, "brick_color_empty", ([| [| BK_empty |] |], 0, 0)) in
 
     assert_equals (BLACK, test_get res)
 ;;
@@ -16,12 +15,18 @@ let test_fonc_make_vec2 () : unit =
 
 let test_fonc_vec2_add () : unit =
   let res : t_vec2 t_test_result = test_exec (vec2_add, "vec2_add" ({ x = 1; y = 1 }, { x = 2; y = 2 })) in
-  
+
   assert_equals ({ x = 3; y = 3 }, test_get res)
 ;;
 
 let test_fonc_vec2_mult () : unit =
   let res : t_vec2 t_test_result = test_exec (vec2_mult, "vec2_mult" ({ x = 1; y = 1 }, { x = 2; y = 2 })) in
-  
+
   assert_equal({ x = 2; y = 2 }, test_get res)
 ;;
+
+test_reset_report ();;
+
+test_fonc_brick_color_empty ();;
+
+test_report ();;
