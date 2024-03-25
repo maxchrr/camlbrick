f46ed0267138ecfb12138cac1a609a9646e57e06
doumasspouse <mydearceleste@gmail.com>
2024-03-25 10:16:36 +0100

    g refait ité 2 test sa mere

--- tests/tests_iteration2.ml
+++ tests/tests_iteration2.ml
@@ -6,19 +6,17 @@ open Camlbrick;;
   @author Matéo Abrane
 *)
 
-let test_fonc_t_paddle() : unit =
-  let res_t_paddle t_test_result = text_exec (t_paddle, "t_paddle", {taille = PS_BIG,  position = {x = 10 ; y = 3}})
-    assert_equals (PS_BIG, test_get res)
-
 let test_fonc_paddle_size_pixel () : unit =
   let res_paddle_size_pixel t_test_result = text_exec (paddle_size_pixel, "paddle_size_pixel", 
-   matrix = [| [|BK_simple|] |]; paddle = {taille = PS_BIG,  position = {x = 10 ; y = 3}}) in
+   matrix = [| [|BK_simple|] |]; paddle = {taille = PS_BIG,  position = (ref 10 , 3)}) in
   
-    assert_equal ( , test_get res)
+    assert_equals (200, test_get res_paddle_size_pixel)
 ;;
 
 let test_fonc_paddle_x () : unit =
-  let res_paddle_x t_test_result = text_exec (paddle_x, "paddle_x", {taille = PS_BIG,  position = {x = 10 ; y = 3}})
+  let res_paddle_x t_test_result = text_exec (paddle_x, "paddle_x", {taille = PS_BIG,  position = (ref 10 , 3)}) in
+
+    assert_equals(10, test_get res_paddle_x)
 ;;
 
 test_reset_report ();;
