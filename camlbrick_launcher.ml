4f52e31067582e397dba1199529abd7af0fa4fa0
Max Charrier <m@lilmaax.fr>
2024-04-16 17:59:58 +0200

    Add brick bonus and fix counter
    Signed-off-by: Max Charrier <m@lilmaax.fr>

--- camlbrick_launcher.ml
+++ camlbrick_launcher.ml
@@ -7,15 +7,22 @@ let param : t_camlbrick_param = param_get game;;
 (* Charger le niveau par défaut ici *)
 
 let tab : t_brick_kind array = [| BK_empty; BK_simple; BK_double |] in
-let count : int ref = ref 0 in
+let count_bonus : int ref = ref 0
+and count_block : int ref = ref 0
+in
 for x = 0 to Array.length game.matrix - 1 do
   for y = 0 to Array.length game.matrix.(x) - 1 do
-    if !count = 50 then
-      game.matrix.(x).(y) <- BK_bonus
-    else
+    if !count_bonus = 50 then begin
+      game.matrix.(x).(y) <- BK_bonus;
+      count_bonus := 0
+    end else if !count_bonus = 50 then begin
+      game.matrix.(x).(y) <- BK_block;
+      count_block := 0
+    end else
       game.matrix.(x).(y) <- tab.(Random.int (Array.length tab));
 
-    count := !count + 1
+    count_bonus := !count_bonus + 1;
+    count_block := !count_block + 1
   done;
 done;
 
