ae0af88dead77088a3a677c2c57e15b1d534f008
Max Charrier <m@lilmaax.fr>
2024-04-16 18:04:41 +0200

    Remove second counter
    Signed-off-by: Max Charrier <m@lilmaax.fr>

--- camlbrick_launcher.ml
+++ camlbrick_launcher.ml
@@ -7,22 +7,18 @@ let param : t_camlbrick_param = param_get game;;
 (* Charger le niveau par défaut ici *)
 
 let tab : t_brick_kind array = [| BK_empty; BK_simple; BK_double |] in
-let count_bonus : int ref = ref 0
-and count_block : int ref = ref 0
-in
+let count : int ref = ref 0 in
 for x = 0 to Array.length game.matrix - 1 do
   for y = 0 to Array.length game.matrix.(x) - 1 do
-    if !count_bonus = 50 then begin
+    if !count = 50 then begin
       game.matrix.(x).(y) <- BK_bonus;
-      count_bonus := 0
-    end else if !count_bonus = 50 then begin
-      game.matrix.(x).(y) <- BK_block;
-      count_block := 0
-    end else
+      count := 0
+    end else if !count = 20 then
+      game.matrix.(x).(y) <- BK_block
+    else
       game.matrix.(x).(y) <- tab.(Random.int (Array.length tab));
 
-    count_bonus := !count_bonus + 1;
-    count_block := !count_block + 1
+    count := !count + 1
   done;
 done;
 
